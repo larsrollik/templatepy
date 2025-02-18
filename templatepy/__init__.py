fbef6f2a77d2170c13879b1174db9609639dc82d
Lars B. Rollik <l.b.rollik@protonmail.com>
2025-02-18 10:28:58 +0000

    Update __init__.py

--- templatepy/__init__.py
+++ templatepy/__init__.py
@@ -1,6 +1,13 @@
 __author__ = "Lars B. Rollik"
 __version__ = "0.2.7"
 
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("templatepy")
+except PackageNotFoundError:
+    # package is not installed
+    pass
 
 def run():
     """Example `run` function for entrypoint in `setup.cfg`"""
