4354f922366a94a369a5cba53182ad2a02699fb9
Jan10e <Jan10e@users.noreply.github.com>
2026-02-21 13:18:56 +0000

    Squash merge from 'main' into 'prod'.

--- templatepy/__init__.py
+++ templatepy/__init__.py
@@ -1,9 +1,10 @@
 __author__ = "Lars B. Rollik"
 
 from importlib.metadata import PackageNotFoundError, version
+from pathlib import Path
 
 try:
-    __version__ = version("subject_weight_db")
+    __version__ = version(Path(__file__).parent.name)
 except PackageNotFoundError:
     __version__ = "0.2.9"
 
